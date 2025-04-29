8af3c6edc277323991c4168916426191a19ca279
Sebas Bautista Navarro <s_bautistanavarro@fanshaweonline.ca>
2025-04-29 18:29:28 -0400

    Fixed Camera and collision bugs that occured after switching to the raylib camera.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include "raymath.h"
 #include <iostream>
 
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
 #define WINDOW_WIDTH 1920/2
 #define WINDOW_HEIGHT 1080/2
 
@@ -38,6 +41,7 @@ int main()
 
 	Texture2D SpaceshipTexture = LoadTexture("assets/images/spaceship.png");
 	Vector2 ShipPos{100, 100};
+	Vector2 ShipDrawPos = ShipPos;
 	Vector2 ShipPrevPos = ShipPos;
 	Vector2 ShipHeading{1, 0};
 	float HeadingAngle = 0;
@@ -47,7 +51,7 @@ int main()
 	float TargetDistance = 0;
 	float DistanceTraveled = 0;
 	bool ShipSelected = false;
-	Rectangle SpacshipRect{ ShipPos.x, ShipPos.y, SpaceshipTexture.width, SpaceshipTexture.height};
+
 
 
 	Vector2 BackGroundPos{ 300, 300 };
@@ -65,13 +69,14 @@ int main()
 	int ZoomDirection = 0;
 	float ZoomSpeed = 1;
 	int RotateDirection = 0;
-	float RotateSpeed = 1;
+	float RotateSpeed = 50;
 
 	while (!WindowShouldClose())
 	{
 	// Input
 		// Mouse Input
 		Vector2 MousePos = GetMousePosition();
+		Vector2 WorldMousePos = GetScreenToWorld2D(MousePos, Camera);
 		bool MOUSE_BUTTON_1_PRESSED = IsMouseButtonPressed(0);
 		bool MOUSE_BUTTON_2_PRESSED = IsMouseButtonPressed(1);
 		bool MOUSE_BUTTON_3_PRESSED = IsMouseButtonPressed(2);
@@ -82,13 +87,16 @@ int main()
 		bool CAMERA_MOVE_DOWN = IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN);
 		bool CAMERA_MOVE_LEFT = IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT);
 		bool CAMERA_MOVE_RIGHT = IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT);
-		bool CAMERA_ZOOM_IN = IsKeyDown(KEY_Z);
-		bool CAMERA_ZOOM_OUT = IsKeyDown(KEY_X);
+		bool CAMERA_ZOOM_IN = IsKeyDown(KEY_PAGE_UP);
+		bool CAMERA_ZOOM_OUT = IsKeyDown(KEY_PAGE_DOWN);
+		bool CAMERA_ROTATE_LEFT = IsKeyDown(KEY_Q);
+		bool CAMERA_ROTATE_RIGHT = IsKeyDown(KEY_E);
 
 		CameraDirection.x = (int(CAMERA_MOVE_LEFT) - int(CAMERA_MOVE_RIGHT));
 		CameraDirection.y = (int(CAMERA_MOVE_UP) - int(CAMERA_MOVE_DOWN));
 		CameraDirection = Vector2Normalize(CameraDirection);
 		ZoomDirection = (int(CAMERA_ZOOM_IN) - int(CAMERA_ZOOM_OUT));
+		RotateDirection = (int(CAMERA_ROTATE_RIGHT) - int(CAMERA_ROTATE_LEFT));
 		
 		// Debug Input
 		if (!DEBUG_MODE)
@@ -106,15 +114,17 @@ int main()
 	// Ai
 
 	// Calculate Positions
-		SpacshipRect.width = SpaceshipTexture.width /** ZoomLevel*/;
-		SpacshipRect.height = SpaceshipTexture.height /** ZoomLevel*/;
-		SpacshipRect.x = ShipPos.x - SpacshipRect.width / 2;
-		SpacshipRect.y = ShipPos.y - SpacshipRect.height / 2;
+
+		Rectangle ShipRect;
+		ShipRect.width = SpaceshipTexture.width;
+		ShipRect.height = SpaceshipTexture.height;
+		ShipRect.x = ShipPos.x;
+		ShipRect.y = ShipPos.y;
 
 		// Collisions
 		
-		if (CheckCollisionPointRec(MousePos, SpacshipRect)) { MouseHover = MOUSE_HOVER_SHIP; }
-		else if (CheckCollisionPointCircle(MousePos, BackGroundPos, 50 /** ZoomLevel*/)) { MouseHover = MOUSE_HOVER_SUN; }
+		if (CheckCollisionPointRec(WorldMousePos, ShipRect)) { MouseHover = MOUSE_HOVER_SHIP; }
+		else if (CheckCollisionPointCircle(WorldMousePos, BackGroundPos, 50)) { MouseHover = MOUSE_HOVER_SUN; }
 		else { MouseHover = NO_VALUE; }
 
 	// Updates
@@ -127,7 +137,7 @@ int main()
 
 		if (TargetSelected == SELECT_SHIP && MOUSE_BUTTON_2_PRESSED)
 		{
-			TargetVector = MousePos;
+			TargetVector = WorldMousePos;
 			ShipAccel = ShipTopSpeed / 10;
 		}
 		
@@ -149,17 +159,20 @@ int main()
 		CameraPosition.x -= CameraDirection.x * CameraSpeed * dt;
 		CameraPosition.y -= CameraDirection.y * CameraSpeed * dt;
 		Camera.zoom += ZoomDirection * ZoomSpeed * dt;
+		Camera.zoom = MAX(0.2, MIN(5, Camera.zoom));
 		Camera.target = CameraPosition;
+		Camera.rotation += RotateDirection * RotateSpeed * dt;
 		
 
 		/*BackGroundPos.x += CameraMovement.x;
 		BackGroundPos.y += CameraMovement.y;*/
 		
-		ShipPos.x += ShipHeading.x * ShipSpeed * dt /** ZoomLevel*//* + CameraMovement.x*/;
-		ShipPos.y += ShipHeading.y * ShipSpeed * dt /** ZoomLevel*/ /*+ CameraMovement.y*/;
+		ShipPos.x += ShipHeading.x * ShipSpeed * dt/* * Camera.zoom*/ /** + CameraMovement.x*/;
+		ShipPos.y += ShipHeading.y * ShipSpeed * dt/* * Camera.zoom*/ /*+ CameraMovement.y*/;
 
 		std::cout << "Pos: (" << ShipPos.x << ", " << ShipPos.y << "), ";
-		std::cout << "MPos: (" << MousePos.x << ", " << MousePos.y << "), ";
+		std::cout << "WMPos: (" << WorldMousePos.x << ", " << WorldMousePos.y << "), ";
+		std::cout << "SMPos: (" << MousePos.x << ", " << MousePos.y << "), ";
 		std::cout << "Spd: " << ShipSpeed << ", ";
 		std::cout <<  "Acl: " << ShipAccel << ", ";
 		std::cout <<  "TD: " << TargetDistance << " MouseHover: " << MouseHover << std::endl;
@@ -168,26 +181,33 @@ int main()
 		BeginDrawing();
 		BeginMode2D(Camera);
 		ClearBackground(BLACK);
-		DrawCircleV(BackGroundPos, 50/**ZoomLevel*/, ORANGE);
+		DrawCircleV(BackGroundPos, 50, ORANGE);
 		//DrawCircleV(MousePos, 5, WHITE);
-		DrawEllipseLines(BackGroundPos.x, BackGroundPos.y, 300/**ZoomLevel*/, 300/** ZoomLevel*/, YELLOW);
+		DrawEllipseLines(BackGroundPos.x, BackGroundPos.y, 300, 300, YELLOW);
 
 		
-		Vector2 ShipDrawPos
+		/*Vector2 ShipDrawPos
 		{ 
-			ShipPos.x - SpaceshipTexture.width / 2 /** ZoomLevel*/,
-			ShipPos.y - SpaceshipTexture.height / 2 /** ZoomLevel*/
-		};
-		DrawTextureEx(SpaceshipTexture, ShipDrawPos, HeadingAngle, 1 /** ZoomLevel*/, WHITE);
+			ShipPos.x - SpaceshipTexture.width / 2,
+			ShipPos.y - SpaceshipTexture.height / 2
+		};*/
+
+		ShipDrawPos = GetWorldToScreen2D(ShipPos, Camera);
+		//Rectangle SpaceshipRectDraw{ShipDrawPos.x, ShipDrawPos.y, SpaceshipTexture.width * Camera.zoom, SpaceshipTexture.height * Camera.zoom };
+		DrawTextureEx(SpaceshipTexture, ShipPos, HeadingAngle, 1, WHITE);
 		
 		if (DEBUG_MODE)
 		{
-			DrawRectangleRec(SpacshipRect, { 255,255,255,100 });
+			DrawRectangleRec(ShipRect, { 255,255,255,100 });
 			DrawFPS(0, 0);
 		}
 		else if (TargetSelected == SELECT_SHIP)
 		{
-			DrawRectangleLinesEx(SpacshipRect, 1, YELLOW);
+			DrawRectangleLinesEx(ShipRect, 1, YELLOW);
+		}
+		else if (TargetSelected == SELECT_SUN)
+		{
+			DrawCircleLinesV(BackGroundPos, 50, WHITE);
 		}
 		EndMode2D();
 		EndDrawing();
